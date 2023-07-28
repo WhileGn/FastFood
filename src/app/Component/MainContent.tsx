3f658908261cf14d49c45da4b38e4ef13d717235
while <while.gn@gmail.com>
2023-07-28 19:15:32 +0330

    update

--- src/app/Component/MainContent.tsx
+++ src/app/Component/MainContent.tsx
@@ -1,14 +1,26 @@
+import { Yatra_One } from "next/font/google";
+
+const roboto = Yatra_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 export default function MainContent() {
   return (
     <>
       <div className=" main__Content">
         <div className=" __Content__  mt-28 h-[20rem] w-[30rem] rounded-[60px] grid items-center justify-items-center duration-300 ">
-          <div className="Name__Content">name</div>
-          <div className="whatis__Content">
-            {" "}
-            about bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+          <div
+            className={`${roboto.className} Name__Content text-red-800 font-bold text-4xl`}
+          >
+            Name
           </div>
-          <div className="price__Content">220 T</div>
+          <div className="whatis__Content text-yellow-100 p-6 self-center ">
+            {" "}
+            about bla bla fsdfdsfsfsfsaoifjsoifsafijsaoifbla bla bla bla bla bla
+            bla bla bla bla bla bla
+          </div>
+          <div className="price__Content text-orange-800 font-bold">220 T</div>
         </div>
       </div>
     </>
